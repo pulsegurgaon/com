5453264029ab64c26d14cfb3391593d89e2eff06
pulsegurgaon <mailbox.rt01@gmail.com>
2026-03-29 00:04:45 +0530

    Create article.js

---
+++ article.js
@@ -0,0 +1,24 @@
+async function loadArticle(){
+
+  const params = new URLSearchParams(window.location.search);
+  const id = params.get("id");
+
+  const res = await fetch("articles.json");
+  const data = await res.json();
+
+  const article = data.articles[id];
+
+  if(!article){
+    document.body.innerHTML = "Article not found";
+    return;
+  }
+
+  document.getElementById("title").innerText = article.title_en;
+  document.getElementById("summary").innerText = article.summary_en;
+  document.getElementById("image").src = article.image;
+
+  document.getElementById("meta").innerText =
+    `${article.category} • ${article.publishedAt}`;
+}
+
+loadArticle();
