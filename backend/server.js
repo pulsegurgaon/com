3382034d02fced60a50d81daab18ed2b191b9575
pulsegurgaon <mailbox.rt01@gmail.com>
2026-04-01 12:50:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -326,6 +326,6 @@ setInterval(()=>{
   fetch(`http://localhost:${PORT}`).catch(()=>{});
 }, 5 * 60 * 1000);
 
-app.listen(PORT,()=>{
-  console.log("Server running on port", PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("🔥 Server running on port", PORT);
 });
